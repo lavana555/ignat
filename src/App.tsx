220a1c65bc16741908de256bee8cb866e4fb91f5
lavana555 <timofeevniki3@gmail.com>
2020-03-31 15:16:45 +0300

    formRegistrNew

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,49 @@ import React from 'react';
 import './App.css';
 import Registration from "./Registration";
 import RecoveryPassContainer from "./RecoveryPass/RecoveryPassContainer";
+import {BrowserRouter, Link, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-    <Registration />
-    <RecoveryPassContainer/>
-    </div>
-  );
+class App extends React.Component {
+
+
+    state = {
+        flag: false
+
+    }
+    showHedaer = () => {
+        this.setState({
+            flag: !this.state.flag
+
+        })
+    }
+
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <div className="header">
+                        <div className="headerbtn" onClick={this.showHedaer}>SHOW HEADER</div>
+                        {this.state.flag &&
+                      <>  <ul className="nav">
+                            <li><Link to="SignIN">sig-in</Link></li>
+                            <li><Link to="Registration">register</Link></li>
+                            <li><Link to="Forgot">RecoveryPassword</Link></li>
+                        </ul>
+                          <Route exact path='/Registration' component={Registration}/>
+                          <Route exact path='/Forgot' component={RecoveryPassContainer}/>
+                      </>
+                        }
+                    </div>
+
+                    {/*<Route exact path='/SignIN' component={SignIN}/>*/}
+
+                    {/*<Registration/>*/}
+                    {/*<RecoveryPassContainer/>*/}
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/Registration.js
+++ src/Registration.js
@@ -4,48 +4,83 @@ import {connect} from "react-redux";
 import {addUserTC} from "./regInReducer";
 
 
-
 class Registration extends React.Component {
 
-    state={
-        email:'',
-        pas:''
+    state = {
+        email: '',
+        pas1: '',
+        pas2: '',
+        errorPas: false,
+        errorMail: false
     }
 
 
-    ChangedEmil=(e)=>{
-       // let newEmail=
+    ChangedEmil = (e) => {
+        // let newEmail=
         this.setState({
-            email:e.currentTarget.value
+            email: e.currentTarget.value
         })
 
     }
 
-    ChangedPass=(e)=>{
+    ChangedPassOne = (e) => {
         this.setState({
-            pas:e.currentTarget.value
+            pas1: e.currentTarget.value
         })
     }
 
-    addRegister=()=>{
-        this.props.addUserTC(this.state.email,this.state.pas)
+    ChangedPassTwo = (e) => {
+        this.setState({
+            pas2: e.currentTarget.value
+        })
+    }
+
+    addRegister = () => {
+        let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (this.state.pas1 !== this.state.pas2) {
+            this.setState({
+                errorPas: !this.state.errorPas
+            })
+        } else if (reg.test(this.state.email) === false) {
+            this.setState({
+                errorMail: !this.state.errorMail
+            })
+
+        } else {
+            this.props.addUserTC(this.state.email, this.state.pas1)
+        }
+
+
     }
 
     render = () => {
-
         return (
-
             <div className={classes.register}>
                 register
-<input onChange={this.ChangedEmil} />
-<input onChange={this.ChangedPass}/>
-<input />
-                <div className={classes.btn} onClick={this.addRegister} >Register</div>
+                mail: <input onChange={this.ChangedEmil}/>
+                passsword: <input onChange={this.ChangedPassOne}/>
+                password: <input onChange={this.ChangedPassTwo}/>
+                {this.state.errorPas ?
+                    <div style={{color: "red"}}>You entered two different passwords. Please try again</div> : null}
+                {this.state.errorMail ?
+                    <div style={{color: "red"}}>You entered incorrect mail. Please try again</div> : null}
+                {this.props.loading ? <div style={{color: "red"}}>Loading...</div> : null}
+                {this.props.error!==''?<div style={{color: "red"}}>{this.props.error}</div> : null}
+                <div className={classes.btn} onClick={this.addRegister}>Register</div>
 
-    </div>
+            </div>
 
-    );
+        );
     }
 }
 
-export default connect(null,{addUserTC})(Registration)
+
+const mapStateToProps = (state) => {
+    return {
+        loading: state.regIn.loading,
+        error:state.regIn.error
+    }
+}
+
+
+export default connect(mapStateToProps, {addUserTC})(Registration)
