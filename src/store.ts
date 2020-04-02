8b0a0f62edd1f5c13d1dd05acb2c09e91a79b953
Roman Lazuka <rn.lazuka@gmail.com>
2020-04-02 13:30:18 +0300

    connected login page to app

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,20 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
-import  thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk"
 import {regInReducer} from "./regInReducer";
 import {recPassReducer} from "./RecoveryPass/bll/recPassReducer";
 import {newPassReducer} from "./NewPass/bll/newPassReducer";
-// import logInReducer from "./Login/login-reducer";
+import logInReducer from "./Login/login-reducer";
 
-const rootReducer=combineReducers({
-    // logIn:logInReducer,
-    regIn:regInReducer,
-    recPass:recPassReducer,
-    newPass:newPassReducer,
+const rootReducer = combineReducers({
+    logIn: logInReducer,
+    regIn: regInReducer,
+    recPass: recPassReducer,
+    newPass: newPassReducer,
     // profile:profileReducer
 })
 
 type RootReducerType = typeof rootReducer
-export type AppStateType=ReturnType<RootReducerType>
+export type AppStateType = ReturnType<RootReducerType>
 
-const store=createStore(rootReducer,applyMiddleware(thunkMiddleware))
-export  default store
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+export default store
